5123940cf4375567687920c424e98d129193e010
traceymmbonelungahi <t.lungahi@gmail.com>
2022-05-31 09:09:52 +0300

    commit tree 91a31495366aa418b906f7ca0bbbc9b31d0c93ef

---
+++ index.js
@@ -0,0 +1,30 @@
+1 + 80;
+60 - 40;
+2 * 3.4;
+5.0 / 2.5;
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+let n = 0;
+function increment(n) {
+    return ++n;
+}
+let p = 5;
+function decrement(p) {
+    return --p;
+}
+function makeInt(a) {
+    return parseInt(a, 10)
+}
+function preserveDecimal(m) {
+    return parseFloat(m)
+}
